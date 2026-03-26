93f880bf0abd3d05ead811f1ba81906a693f3967
Ziad-Alghawi <ziad97algawy@gmail.com>
2026-03-26 18:55:44 +0100

    server to use ES modules and add Routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
-const express = require('express');
+import express from 'express';
 
+// Import Routes
+import movieRoutes from './routes/movieRoutes.js';
+
+// Create Express app
 const app = express();
 
+// API Routes
+app.use("/movies", movieRoutes);
+
+
+// Start the server
 const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
